9d2c7fd10d00839fbb322bdd735480e3a310d02d
MinChatr <min_etc@hotmail.com>
2025-02-08 13:46:44 +0700

    just format code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 console.log("Hello, TypeScript with Node.js!")
-
 // Task 2 : การใช้งาน Data Type ของ TypeScript
 // let x : number | string = 10; 
 // x = 'hello'; 
@@ -13,19 +12,18 @@ console.log("Hello, TypeScript with Node.js!")
 // }
 
 // Task 3 : การเรียกใช้งาน Function ใน TypeScript
-
 const add = (a : number ,b : number) : string => { 
     const result = a+b; 
     return result.toString(); 
 }
-
 // Function to find the maximum value in a list of numbers and return it as a string
 const findMax = (numbers: number[]): string => {
     const maxNumber = Math.max(...numbers);
     return maxNumber.toString();
-}
 
 // Example usage
 const numbers = [1, 2, 3, 4, 5];
 console.log(`The maximum number is: ${findMax(numbers)}`);
 console.log(typeof findMax(numbers));
+
+// Task 4 : การ import export function 
