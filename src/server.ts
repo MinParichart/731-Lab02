6f552a6a781e582c79856b57682853772246ce55
MinChatr <min_etc@hotmail.com>
2025-02-12 09:51:29 +0700

    Task 10 Question3 - create endpoint for query data

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,51 @@
-// Task 8 การใช้ express เพื่อสร้าง server เบื้องต้น 
+// Task 8 การใช้ express เพื่อสร้าง server เบื้องต้น
 // Task 8 ข้อ2
 // import express, { Request, Response } from 'express'
-// const app = express() 
+// const app = express()
 // const port = 3000
 
 // app.get('/',(req : Request, res : Response) => {
 //     res.send('Hello World')
 // })
 
-// app.listen(port, () => { 
+// app.listen(port, () => {
 //     console.log(`App listening at http : //localhost:${port}`)
 // })
 
 // Task 8 ข้อ7
 // import express, { Request, Response } from 'express'
-// const app = express() 
+// const app = express()
 // const port = 3000
 
 // app.get('/',(req : Request, res : Response) => {
 //     res.send('Hello World')
 // })
 
-// app.listen(port, () => { 
+// app.listen(port, () => {
 //     console.log(`App listening at http : //localhost:${port}`)
 // })
 
-// app.get('/test', (req, res) => { 
+// app.get('/test', (req, res) => {
 //     res.send('Hello World!')
 // })
 
 // Task 8 ข้อ8
 // import express, { Request, Response } from 'express'
-// const app = express() 
+// const app = express()
 // const port = 3000
 
 // app.get('/',(req : Request, res : Response) => {
 //     res.send('Hello World')
 // })
 
-// app.listen(port, () => { 
+// app.listen(port, () => {
 //     console.log(`App listening at http : //localhost:${port}`)
 // })
 
-// app.get('/test', (req, res) => { 
-//     let returnObj = { 
-//         name : 'test', 
-//         age : 20, 
+// app.get('/test', (req, res) => {
+//     let returnObj = {
+//         name : 'test',
+//         age : 20,
 //         address : 'Thai'
 //     }
 //     res.send(returnObj);
@@ -53,125 +53,155 @@
 
 // Task 8 ข้อ9
 // import express, { Request, Response } from 'express'
-// const app = express() 
+// const app = express()
 // const port = 3000
 
 // app.get('/',(req : Request, res : Response) => {
 //     res.send('Hello World')
 // })
 
-// app.listen(port, () => { 
+// app.listen(port, () => {
 //     console.log(`App listening at http : //localhost:${port}`)
 // })
 
-// app.get('/test', (req : Request, res : Response) => { 
+// app.get('/test', (req : Request, res : Response) => {
 //     const id = req.query.id;
-//     const output = `id : ${id}`; 
+//     const output = `id : ${id}`;
 //     res.send(output);
 // })
 
-// Task 9 ข้อ1 สร้าง type ของ Object ที่จะใช้ใน server.ts โดยการเพิ่ม code นี้ 
-import express, { Request, Response } from 'express'
-const app = express() 
-const port = 3000
+// Task 9 ข้อ1 สร้าง type ของ Object ที่จะใช้ใน server.ts โดยการเพิ่ม code นี้
+// import express, { Request, Response } from 'express'
+// const app = express()
+// const port = 3000
 
-app.get('/',(req : Request, res : Response) => {
-    res.send('Hello World')
-})
+// app.get('/',(req : Request, res : Response) => {
+//     res.send('Hello World')
+// })
 
-app.listen(port, () => { 
-    console.log(`App listening at http : //localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`App listening at http : //localhost:${port}`)
+// })
 
-app.get('/test', (req : Request, res : Response) => { 
-    const id = req.query.id;
-    const output = `id : ${id}`; 
-    res.send(output);
-})
+// app.get('/test', (req : Request, res : Response) => {
+//     const id = req.query.id;
+//     const output = `id : ${id}`;
+//     res.send(output);
+// })
 
-app.get('/event', (req : Request, res : Response) => { 
-    res.json(events);
-})
-interface Event { 
-    id : number; 
-    category : string; 
-    title : string; 
-    decription : string; 
-    location : string; 
-    date : string; 
-    time : string; 
-    petsAllowed : boolean; 
-    organizer : string; 
+// app.get('/events', (req : Request, res : Response) => {
+//     res.json(events);
+// })
+
+// Task 10 การสร้าง endpoint เพื่อ query ข้อมูล
+// Task 10 ข้อ1
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(events);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http : //localhost:${port}`);
+});
+
+// กรณีไม่มีการ check ค่า (key:"value") ว่ามีการส่งมาหรือไม่
+// app.get("/events", (req, res) => {
+//     const category = req.query.category;
+//     const filteredEvents = events.filter((event) => event.category === category);
+//     res.json(filteredEvents);
+// });
+
+// กรณีมีการ check ค่า (key:"value") ว่ามีการส่งมาหรือไม่
+app.get("/events", (req, res) => {
+  if (req.query.category) {
+    const category = req.query.category; 
+    const filteredEvents = events.filter((event) => event.category === category); 
+    res.json(filteredEvents); 
+  } else { 
+    res.json (events); 
+  }
+});
+interface Event {
+  id: number;
+  category: string;
+  title: string;
+  decription: string;
+  location: string;
+  date: string;
+  time: string;
+  petsAllowed: boolean;
+  organizer: string;
 }
 
-// Task 9 ข้อ2 สร้าง ตัวแปร events เพื่อเก็บข้อมูล list ของ event 
+// Task 9 ข้อ2 สร้าง ตัวแปร events เพื่อเก็บข้อมูล list ของ event
 const events : Event[] = [
-    { 
-        id : 1, 
-        category : "Music", 
-        title : "Concert", 
-        decription : "A live concert", 
-        location : "London", 
-        date : "2021-07-01", 
-        time : "19:00", 
-        petsAllowed : false, 
-        organizer : "Live Nation", 
-    }, 
-    { 
-        id : 2, 
-        category : "Music", 
-        title : "Festival", 
-        decription : "A music festival", 
-        location : "Manchester", 
-        date : "2021-07-15", 
-        time : "12:00", 
-        petsAllowed : true, 
-        organizer : "Festival Republic", 
-    }, 
-    { 
-        id : 3, 
-        category : "Music", 
-        title : "Gig", 
-        decription : "A gig", 
-        location : "Birmingham", 
-        date : "2021-07-30", 
-        time : "20:00", 
-        petsAllowed : true, 
-        organizer : "Academy Music Group", 
-    },
-    {
-        id: 4,
-        category: "Music",
-        title: "Concert",
-        decription: "A live concert",
-        location: "London",
-        date: "2021-07-01",
-        time: "19:00",
-        petsAllowed: false,
-        organizer: "Live Nation",
-    },
-    {
-        id: 5,
-        category: "Music",
-        title: "Festival",
-        decription: "A music festival",
-        location: "Manchester",
-        date: "2021-07-15",
-        time: "12:00",
-        petsAllowed: true,
-        organizer: "Festival Republic",
-    },
-    {
-        id: 6,
-        category: "Music",
-        title: "Gig",
-        decription: "A gig",
-        location: "Birmingham",
-        date: "2021-07-30",
-        time: "20:00",
-        petsAllowed: true,
-        organizer: "Academy Music Group",
-    }
-]
-
-
+  {
+    id: 1,
+    category: "Music",
+    title: "Concert",
+    decription: "A live concert",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 2,
+    category: "Music",
+    title: "Festival",
+    decription: "A music festival",
+    location: "Manchester",
+    date: "2021-07-15",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Festival Republic",
+  },
+  {
+    id: 3,
+    category: "Music",
+    title: "Gig",
+    decription: "A gig",
+    location: "Birmingham",
+    date: "2021-07-30",
+    time: "20:00",
+    petsAllowed: true,
+    organizer: "Academy Music Group",
+  },
+  {
+    id: 4,
+    category: "Sports",
+    title: "Football Match",
+    decription: "A live football match",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Premier League",
+  },
+  {
+    id: 5,
+    category: "Music",
+    title: "Festival",
+    decription: "A music festival",
+    location: "Manchester",
+    date: "2021-07-15",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Festival Republic",
+  },
+  {
+    id: 6,
+    category: "Music",
+    title: "Gig",
+    decription: "A gig",
+    location: "Birmingham",
+    date: "2021-07-30",
+    time: "20:00",
+    petsAllowed: true,
+    organizer: "Academy Music Group",
+  },
+];
