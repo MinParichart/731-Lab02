d2b5b635e1c73ce53695db059c869984aee2f64e
MinChatr <min_etc@hotmail.com>
2025-02-20 15:10:50 +0700

    Task 1 Lab 3

--- src/server.ts
+++ src/server.ts
@@ -138,35 +138,176 @@
 
 // Task 11 การสร้าง Endpoint เพื่อเพิ่มข้อมูล
 // Task 11 ข้อ1
+// import express, { Request, Response } from "express";
+// const app = express();
+// app.use(express.json());
+// const port = 3000;
+
+// app.post("/events", (req,res)=> {
+//     const newEvent : Event = req.body; 
+//     newEvent.id = events.length + 1; 
+//     events.push(newEvent); 
+//     res.json(newEvent);
+// });
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send(events);
+// });
+
+// app.listen(port, () => {
+//   console.log(`App listening at http : //localhost:${port}`);
+// });
+
+// // กรณีมีการ check ค่า (key:"value") ว่ามีการส่งมาหรือไม่
+// app.get("/events", (req, res) => {
+//   if (req.query.category) {
+//     const category = req.query.category; 
+//     const filteredEvents = events.filter((event) => event.category === category); 
+//     res.json(filteredEvents); 
+//   } else { 
+//     res.json (events); 
+//   }
+// });
+
+// interface Event {
+//   id: number;
+//   category: string;
+//   title: string;
+//   decription: string;
+//   location: string;
+//   date: string;
+//   time: string;
+//   petsAllowed: boolean;
+//   organizer: string;
+// }
+
+// // Task 9 ข้อ2 สร้าง ตัวแปร events เพื่อเก็บข้อมูล list ของ event
+// const events : Event[] = [
+//   {
+//     id: 1,
+//     category: "Music",
+//     title: "Concert",
+//     decription: "A live concert",
+//     location: "London",
+//     date: "2021-07-01",
+//     time: "19:00",
+//     petsAllowed: false,
+//     organizer: "Live Nation",
+//   },
+//   {
+//     id: 2,
+//     category: "Music",
+//     title: "Festival",
+//     decription: "A music festival",
+//     location: "Manchester",
+//     date: "2021-07-15",
+//     time: "12:00",
+//     petsAllowed: true,
+//     organizer: "Festival Republic",
+//   },
+//   {
+//     id: 3,
+//     category: "Music",
+//     title: "Gig",
+//     decription: "A gig",
+//     location: "Birmingham",
+//     date: "2021-07-30",
+//     time: "20:00",
+//     petsAllowed: true,
+//     organizer: "Academy Music Group",
+//   },
+//   {
+//     id: 4,
+//     category: "Sports",
+//     title: "Football Match",
+//     decription: "A live football match",
+//     location: "London",
+//     date: "2021-07-01",
+//     time: "19:00",
+//     petsAllowed: false,
+//     organizer: "Premier League",
+//   },
+//   {
+//     id: 5,
+//     category: "Music",
+//     title: "Festival",
+//     decription: "A music festival",
+//     location: "Manchester",
+//     date: "2021-07-15",
+//     time: "12:00",
+//     petsAllowed: true,
+//     organizer: "Festival Republic",
+//   },
+//   {
+//     id: 6,
+//     category: "Music",
+//     title: "Gig",
+//     decription: "A gig",
+//     location: "Birmingham",
+//     date: "2021-07-30",
+//     time: "20:00",
+//     petsAllowed: true,
+//     organizer: "Academy Music Group",
+//   },
+// ];
+
+// Task 1 Lab3 ------------------------------------------------------
 import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.post("/events", (req,res)=> {
-    const newEvent : Event = req.body; 
-    newEvent.id = events.length + 1; 
-    events.push(newEvent); 
-    res.json(newEvent);
-});
+function getEventBycategory(category:string) : Event[] {
+  const filteredEvents = events.filter((event) => event.category === category);
+  return filteredEvents;
+ }
+
+function getAllEvents() : Event[]{
+  return events;
+}
+
+function getEventByID(id : number) : Event | undefined {
+  return events.find((event) => event.id === id);
+}
+
+function addEvent(newEvent : Event) : Event {
+  newEvent.id = events.length+1;
+  events.push(newEvent);
+  return newEvent;
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send(events);
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http : //localhost:${port}`);
+app.get("/events", (req,res) => {
+  if (req.query.category){
+    const category = req.query.category as string;
+    const filteredEvents = getEventBycategory(category as string);
+    res.json(filteredEvents);
+  } else {
+    res.json(getAllEvents);
+  }
+})
+
+app.get("/events/:id", (req,res)=>{
+  const id = parseInt(req.params.id);
+  const event = getEventByID(id);
+  if (event){
+    res.json(event);
+  } else {
+    res.status(404).send("Event not found");
+  }
+})
+
+app.post("/events", (req,res)=>{
+  const newEvent : Event = req.body;
+  addEvent(newEvent);
+  res.json(newEvent);
 });
 
-// กรณีมีการ check ค่า (key:"value") ว่ามีการส่งมาหรือไม่
-app.get("/events", (req, res) => {
-  if (req.query.category) {
-    const category = req.query.category; 
-    const filteredEvents = events.filter((event) => event.category === category); 
-    res.json(filteredEvents); 
-  } else { 
-    res.json (events); 
-  }
+app.listen(port, () => {
+console.log(`App listening at http : //localhost:${port}`);
 });
 
 interface Event {
@@ -181,7 +322,6 @@ interface Event {
   organizer: string;
 }
 
-// Task 9 ข้อ2 สร้าง ตัวแปร events เพื่อเก็บข้อมูล list ของ event
 const events : Event[] = [
   {
     id: 1,
