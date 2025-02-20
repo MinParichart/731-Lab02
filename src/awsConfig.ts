ee228876891e18163ca8c1a4816215889c0b368f
MinChatr <min_etc@hotmail.com>
2025-02-20 23:39:46 +0700

    Task 12 Lab 3 - create .env

--- src/awsConfig.ts
+++ src/awsConfig.ts
@@ -1,12 +1,36 @@
 // Task 9 Lab 3 --------------------------------------------------------------
+// import { S3Client } from "@aws-sdk/client-s3";
+
+// export const s3Client = new S3Client({
+//   credentials: {
+//     accessKeyId: "c2c9bd6d0b321ded6dfc706edf708f3e",
+//     secretAccessKey: "88e4d553b4de899ca51b4f7d7adf917d907b58d7986282ddc488376020fa4f3c",
+//   },
+//   endpoint: "https://xqkgmbsjbovbjcxmxzrx.supabase.co/storage/v1/s3",
+//   region: "ap-southeast-1",
+//   forcePathStyle: true,
+// });
+
+// Task 12 Lab 3 --------------------------------------------------------------
 import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const endpoint = process.env.SUPABASE_ENDPOINT_URL;
+const region = process.env.AWS_REGION;
+
+if (!accessKeyId || !secretAccessKey || !endpoint || !region) {
+    throw new Error("Missing required environment variables for AWS S3 configuration");
+  }
 
 export const s3Client = new S3Client({
   credentials: {
-    accessKeyId: "c2c9bd6d0b321ded6dfc706edf708f3e",
-    secretAccessKey: "88e4d553b4de899ca51b4f7d7adf917d907b58d7986282ddc488376020fa4f3c",
+    accessKeyId,
+    secretAccessKey,
   },
-  endpoint: "https://xqkgmbsjbovbjcxmxzrx.supabase.co/storage/v1/s3",
-  region: "ap-southeast-1",
+  endpoint,
+  region,
   forcePathStyle: true,
 });
--- src/service/uploadFileService.ts
+++ src/service/uploadFileService.ts
@@ -53,6 +53,43 @@
 // }
 
 // Task 11 Lab 3 --------------------------------------------------------------
+// import { PutObjectCommand } from "@aws-sdk/client-s3";
+// import { randomBytes } from "crypto";
+// import { s3Client } from '../awsConfig';
+
+// function generateSaltedFilename(originalName: string): string {
+//   const salt = randomBytes(16).toString("hex");
+//   const extension = originalName.split(".").pop();
+//   return `${salt}.${extension}`;
+// }
+
+// export async function uploadFile(
+//   bucket: string,
+//   filePath: string,
+//   file: Express.Multer.File
+// ): Promise<string> {
+//   const saltedFilename = generateSaltedFilename(file.originalname);
+//   const saltedFilePath = `${filePath}/${saltedFilename}`;
+//   const params = {
+//     Bucket: bucket,
+//     Key: saltedFilePath,
+//     Body: file.buffer,
+//     ContentType: file.mimetype,
+//   };
+
+//   try {
+//     const data = await s3Client.send(new PutObjectCommand(params));
+//     console.log("File uploaded successfully:", data);
+//     const publicUrl = `https://xqkgmbsjbovbjcxmxzrx.supabase.co/storage/v1/object/public/image/${saltedFilePath}`;
+//     console.log("File uploaded successfully:", publicUrl);
+//     return publicUrl;
+//   } catch (error) {
+//     console.error("Error uploading file:", error);
+//     return "error";
+//   }
+// }
+
+// Task 12 Lab 3 --------------------------------------------------------------
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { randomBytes } from "crypto";
 import { s3Client } from '../awsConfig';
@@ -80,7 +117,7 @@ export async function uploadFile(
   try {
     const data = await s3Client.send(new PutObjectCommand(params));
     console.log("File uploaded successfully:", data);
-    const publicUrl = `https://xqkgmbsjbovbjcxmxzrx.supabase.co/storage/v1/object/public/image/${saltedFilePath}`;
+    const publicUrl = `${process.env.SUPABASE_OUTPUT_URL}/${saltedFilePath}`;
     console.log("File uploaded successfully:", publicUrl);
     return publicUrl;
   } catch (error) {
